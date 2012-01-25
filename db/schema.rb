fca58374a855730ed09a0f35f0eb73ca8db31995
jvn <jvn@mikiaholding.dk>
2012-01-25 21:05:09 +0100

    form og show rettet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120125182431) do
+ActiveRecord::Schema.define(:version => 20120125184726) do
+
+  create_table "import_defs", :force => true do |t|
+    t.string   "headline"
+    t.string   "field"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "import_jobs", :force => true do |t|
     t.string   "name"
