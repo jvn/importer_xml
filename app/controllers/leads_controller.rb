d228b172a2959a7c6b9ef649883df8f916364529
jvn <jvn@mikiaholding.dk>
2012-01-25 19:53:56 +0100

    csv til lead

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,16 +1,73 @@
 class LeadsController < ApplicationController
   # GET /leads
   # GET /leads.json
-
+  # encoding: UTF-8
   def csv
     require 'csv'
+    @Firmanavn = 0
+    @Adresse = 1
+    @Postnr = 2
+    @Telefon = 3
+    @Primar_branche = 4
+    @Email = 5
+    @Tilladelse_til = 6
+    @Antal_tilladelser = 7
+    @DTL_Regioner = 8
+    @Antal_biler = 9
+    @Antal_chaufforer = 10
+    @Kunde = 11
+    @Konkurrent = 12
+    @Dorcanvas = 13
+    @Telefoncanvas_EGO = 14
+    @Telefoncanvas_outsourcet = 15
+    @M1_Kontakt_etableret = 16
+    @M2_Mode_booket = 17
+    @M3_Mode_gennemfort = 18
+    @M4_Aftale_indgaet = 19
+    @M5_Deltager_booket = 20
+    @Milepalsstatus = 21
+    @Naste_kontakt = 22
+    @Kommentarer_og_noter = 23
 
     csv_text = File.read('godslisten.csv')
     csv = CSV.parse(csv_text, :headers => true)
     csv.each do |row|
-      row = row.to_hash.with_indifferent_access
-      Lead.create!(row.to_hash.symbolize_keys)
+      #row = row.to_hash.with_indifferent_access
+      @ny = Lead.new
+      @ny.Firmanavn = row[@Firmanavn]
+      logger.info(row)
+      @ny.Adresse = row[@Adresse]
+      @ny.Postnr = row[@Postnr]
+      @ny.Telefon = row[@Telefon]
+      @ny.Primar_branche = row[@Primar_branche]
+      @ny.Email = row[@Email]
+      @ny.Tilladelse_til = row[@Tilladelse_til]
+      @ny.Antal_tilladelser = row[@Antal_tilladelser]
+      @ny.DTL_Regioner = row[@DTL_Regioner]
+      @ny.Antal_biler = row[@Antal_biler]
+      @ny.Antal_chaufforer = row[@Antal_chaufforer]
+      @ny.Kunde = row[@Kunde]
+      @ny.Konkurrent = row[@Konkurrent]
+      @ny.Dorcanvas = row[@Dorcanvas]
+      @ny.Telefoncanvas_EGO = row[@Telefoncanvas_EGO]
+      @ny.Telefoncanvas_outsourcet = row[@Telefoncanvas_outsourcet]
+      @ny.M1_Kontakt_etableret = row[@M1_Kontakt_etableret]
+      @ny.M2_Mode_booket = row[@M2_Mode_booket]
+      @ny.M3_Mode_gennemfort = row[@M3_Mode_gennemfort]
+      @ny.M4_Aftale_indgaet = row[@M4_Aftale_indgaet]
+      @ny.M5_Deltager_booket = row[@M5_Deltager_booket]
+      @ny.Milepalsstatus = row[@Milepalsstatus]
+      @ny.Naste_kontakt = row[@Naste_kontakt]
+      @ny.Kommentarer_og_noter = row[@Kommentarer_og_noter]
+      @ny.save
+      logger.info (@ny.to_yaml)
     end
+
+
+
+
+      #Lead.create!(row.to_hash.symbolize_keys)
+    #end
     redirect_to :root
   end
 
